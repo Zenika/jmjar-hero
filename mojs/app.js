f5dcc80867de1a9a7bfaddcf8ca643bfd2c0fb0a
Martin Mouterde <martin.mouterde@gmail.com>
2017-04-13 21:56:03 +0200

    add integration with angular VS player and score

--- mojs/app.js
+++ mojs/app.js
@@ -1,21 +1,25 @@
 angular.module('app', []).controller('MainController', function ($scope) {
     let main = this;
-    main.totalScore = 0;
-    main.score = 0;
     main.highScores = [];
 
     try {
         if (localStorage.getItem("hightScore")) {
             main.highScores = JSON.parse(localStorage.getItem("hightScore"));
+            main.highScores = main.highScores.sort(sortScores);
         }
     } catch (e) {
         //nop
     }
 
     main.songs = [{name: "Oxygen 4", path: "../music/OXYGENE_4_short.mp3"}];
+
     main.currentTitle = main.songs[0].path;
+    main.currentName = "";
+
     main.onPlay = function () {
-        if (main.currentPlayer && main.currentPlayer.length > 0) {
+        if (main.currentName && main.currentName.length > 0) {
+            main.currentPlayer = {score: 0, name: main.currentName, fails: 0, rank: main.highScores.length};
+            main.highScores.push(main.currentPlayer);
             let audio = new Audio(main.currentTitle);
             audio.addEventListener("play", function () {
                 window.timeline.play();
@@ -25,24 +29,29 @@ angular.module('app', []).controller('MainController', function ($scope) {
             main.isError = true;
         }
     };
+
     main.onNameChange = function () {
-        if (main.currentPlayer && main.currentPlayer.length > 0) {
+        if (main.currentName && main.currentName.length > 0) {
             main.isError = false;
         }
     };
-    $scope.$on("timeline-completed", function (event, score) {
-        main.highScores.push({
-            player: main.currentPlayer,
-            value: score
-        });
-        main.highScores = sortScores(main.highScores);
+
+    $scope.$on("timeline-completed", function (event) {
+        main.highScores = main.highScores.sort(sortScores);
         localStorage.setItem("hightScore", JSON.stringify(main.highScores));
-        totalScore = 0;
     });
 
-    function sortScores(lastHighScore) {
-        return _.sortBy(lastHighScore, function (player) {
-            return player.value.score;
-        }, false);
+    $scope.$on("scope-updated", function (event, scoreToAdd) {
+        main.currentPlayer.score += scoreToAdd;
+        main.highScores = main.highScores.sort(sortScores);
+        main.currentPlayer.rank = main.highScores.indexOf(main.currentPlayer);
+    });
+
+    function sortScores(player1, player2) {
+        if (player1.score === player2.score) {
+            return player1.fails > player2.fails;
+        } else {
+            return player1.score < player2.score;
+        }
     }
 });
--- mojs/score.js
+++ mojs/score.js
@@ -1,30 +1,31 @@
 (function () {
-    window.totalScore = 0;
 
     function score(input) {
         const reactionTime = Math.abs(input.inputTime - input.perfectTime);
-        let score;
+        let scoreLabel;
+        let scoreToAdd = 0;
         if (reactionTime <= 100) {
-            score = '120pts';
-            totalScore += 120;
+            scoreLabel = '120pts';
+            scoreToAdd = 120;
         } else if (reactionTime > 100 && reactionTime <= 250) {
-            score = '100pts';
-            totalScore += 100;
+            scoreLabel = '100pts';
+            scoreToAdd = 100;
         } else if (reactionTime > 250 && reactionTime <= 400) {
-            score = '80pts';
-            totalScore += 80;
+            scoreLabel = '80pts';
+            scoreToAdd = 80;
         } else if (reactionTime > 400 && reactionTime <= 550) {
-            score = '60pts';
-            totalScore += 60;
+            scoreLabel = '60pts';
+            scoreToAdd = 60;
         } else if (reactionTime > 550 && reactionTime <= 700) {
-            score = '40pts';
-            totalScore += 40;
+            scoreLabel = '40pts';
+            scoreToAdd = 40;
         } else if (reactionTime > 700 && reactionTime <= 850) {
-            score = '20pts';
-            totalScore += 20;
+            scoreLabel = '20pts';
+            scoreToAdd = 20;
         }
-
-        displayScore(score);
+        let scope = angular.element(document.body).scope();
+        scope.$apply(scope.$broadcast('scope-updated', scoreToAdd));
+        displayScore(scoreLabel);
     }
 
     function displayScore(score) {
@@ -44,11 +45,7 @@
                 scoreDiv.innerText = '';
             }
         }).play();
-
-        const totalScoreDiv = document.getElementById('totalScore');
-        totalScoreDiv.innerText = totalScore;
     }
 
-    displayScore(totalScore);
     window.score = score;
 })();
