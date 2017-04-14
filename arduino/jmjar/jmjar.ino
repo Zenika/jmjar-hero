456be87cd1e5fc6dcc81f6b6423f0b1d49014ccf
srevereault <sylvain@revereault.fr>
2017-04-14 11:07:26 +0200

    Boucle sur 4 capteurs - TODO modifier back car le signal envoyé a changé

--- arduino/jmjar/jmjar.ino
+++ arduino/jmjar/jmjar.ino
@@ -1,8 +1,7 @@
 /* Utilisation du capteur Ultrason HC-SR04 */
 
 // définition des broches utilisées
-int trig = 11;
-int echo = 12;
+int trig = 2;
 long lecture_echo;
 long cm;
 
@@ -10,25 +9,26 @@ void setup()
 {
   pinMode(trig, OUTPUT);
   digitalWrite(trig, LOW);
-  pinMode(echo, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  lecture_echo = pulseIn(echo, HIGH);
-  cm = lecture_echo / 58;
-  if (cm < 8) {
-    Serial.println("Do");
-  }
-  else if (cm < 16) {
-    Serial.println("Re");
-  }
-  else if (cm < 30) {
-    Serial.println("Mi");
+  for (int echo = 6 ; echo <= 9; echo++){
+    //int echo = 6;
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    lecture_echo = pulseIn(echo, HIGH, 2000);
+    cm = lecture_echo / 58;
+    //Serial.println(cm);
+    if ((cm > 0) and (cm < 10)) {
+       Serial.println(echo);
+    }
   }
   delay(100);
 }
