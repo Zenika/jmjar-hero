f3fff97357554d4c72271416214e1482faba49f1
srevereault <sylvain@revereault.fr>
2017-04-17 17:26:52 +0200

    Passage à 6 entrées et 50 cm de distance

--- arduino/jmjar/jmjar.ino
+++ arduino/jmjar/jmjar.ino
@@ -9,16 +9,18 @@ void setup()
 {
   pinMode(trig, OUTPUT);
   digitalWrite(trig, LOW);
+  pinMode(5, INPUT);
   pinMode(6, INPUT);
   pinMode(7, INPUT);
   pinMode(8, INPUT);
   pinMode(9, INPUT);
+  pinMode(10, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  for (int echo = 6 ; echo <= 9; echo++){
+  for (int echo = 5 ; echo <= 10; echo++){
     //int echo = 6;
     digitalWrite(trig, HIGH);
     delayMicroseconds(10);
@@ -26,7 +28,7 @@ void loop()
     lecture_echo = pulseIn(echo, HIGH, 2000);
     cm = lecture_echo / 58;
     //Serial.println(cm);
-    if ((cm > 0) and (cm < 10)) {
+    if ((cm > 0) and (cm < 50)) {
        Serial.println(echo);
     }
   }
